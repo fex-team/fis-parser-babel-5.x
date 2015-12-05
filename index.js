4267f3f2941c33fc2624d160b48caeb26ea2871e
hefangshi <hefangshi@gmail.com>
2015-12-05 23:18:47 +0800

    conf: use breakConfig to prevent load global .babelrc

--- index.js
+++ index.js
@@ -3,6 +3,10 @@
 var babel = require('babel-core');
 
 module.exports = function (content, file, conf) {
+    // 默认不加载.babelrc，避免错误的加载了全局配置而非编译工具中指定的配置
+    if (conf.breakConfig === undefined) {
+        conf.breakConfig = true;
+    }
     var result = babel.transform(content, conf);
     return result.code;
 };
